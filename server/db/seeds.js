2974b7d50ea0e195ffd0b00bdf4067288359d1ce
yungmanny26 <tondreau24@gmail.com>
2022-07-21 10:09:53 -0400

    low

--- server/db/seeds.js
+++ server/db/seeds.js
@@ -10,6 +10,7 @@ const products = [
     {
       image: [
       "https://res.cloudinary.com/dygxpwdnk/image/upload/v1656167048/little-posite-one-little-kids-shoes-MFQT9P_jya4re.jpg",
+
       ],
       name: "Nike posite one",
       details:
@@ -20,6 +21,9 @@ const products = [
     {
       image: [
         "https://res.cloudinary.com/dygxpwdnk/image/upload/v1656167038/air-force-1-07-next-nature-shoes-Kwpn0q_a2nrsg.jpg",
+        "https://res.cloudinary.com/dygxpwdnk/image/upload/v1658331553/https_3A_2F_2Fhypebeast.com_2Fimage_2F2022_2F04_2Fnike-air-force-1-low-snakeskin-dv1031-030-release-info-000_rhrdhw.jpg",
+       "https://res.cloudinary.com/dygxpwdnk/image/upload/v1658331542/custom-nike-air-force-1-low-by-you-shoe_pn3ynz.jpg",
+        "https://res.cloudinary.com/dygxpwdnk/image/upload/v1658331547/air-force-1-07-premium-mens-shoes-QRS4Fd_bk71dg.jpg",
       ],
       name: "Air force one's ",
       details:
@@ -28,16 +32,18 @@ const products = [
     },
 
     {
-      image: ["https://res.cloudinary.com/dygxpwdnk/image/upload/w_1000,ar_16:9,c_fill,g_auto,e_sharpen/v1656167029/jordan-melo-m12-555088_062_a_prem-e1575044740922_m0sver.jpg"
-        ,
-        
+      image: ["https://res.cloudinary.com/dygxpwdnk/image/upload/w_1000,ar_16:9,c_fill,g_auto,e_sharpen/v1656167029/jordan-melo-m12-555088_062_a_prem-e1575044740922_m0sver.jpg",
+        "https://res.cloudinary.com/dygxpwdnk/image/upload/v1658331769/baskets-air-jordan-1-retro-high-university-blue-air-jordan-kikikickz-441388_1024x1024_jvl92c.jpg",
+        "https://res.cloudinary.com/dygxpwdnk/image/upload/v1658331751/air-jordan-1-high-85-college-navy-bq4422-400-release-date_p5plbw.jpg",
        
       ],
-      name: "jordan's one Exclusive",
+      name: "jordan's one ",
       details:
         "size 6-10",
       price: 250,
     },
+
+
     {
       image: [
         "https://res.cloudinary.com/dro7gwdx3/image/upload/v1654465987/earphones_a_1_ftbism.webp",
@@ -48,6 +54,8 @@ const products = [
         "Get ready to redefine fitness fashion and utility with the wired headphone boAt Bassheads 242, it’s time to get your sport on. It carries ear hooks that are crafted from silicon to maximize comfort and a secure fit that allows you to maximize your workouts. Coated with IPX 4 sweat and water protection, the Bassheads 242 emphasizes on all-around ability with 10mm drivers that reproduce a quality sound. Move around freely with the superior coated cable that reaches comfortably from your pocket till your ears. Connect to a number of devices with ease using the 3.5mm straight jack, because there’s no stress, only sweetness when you’re riding the waves on this boAt. You can tune in and out with the pause/play button that can also take calls (With In-line Mic.) and control volume in-between exercise reputations.",
       price: 34,
     },
+
+    
     {
       image: [
         "https://res.cloudinary.com/dro7gwdx3/image/upload/v1654465989/watch_1_c3zkt7.webp",
